95dd5764e320ba8e708e60674bf0034f74094742
Timur_hnimdvi <timurbakibayev@gmail.com>
2018-08-27 14:01:03 +0600

    small changes to lat lon

--- back/navi/views.py
+++ back/navi/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 def update(request, code):
     text = "updated for code "+str(code) + " lat: " + request.GET.get("lat","0.0")
+    if not (request.GET.get("lat","0") == "0" or request.GET.get("lon", "0") == "0"):
+        return render(request, 'ok.html', context={"text": "Lattitude (lat) or Longitude (lon) is missing :("})
+    
     return render(request, 'ok.html', context={"text": text})
