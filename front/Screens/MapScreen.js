7867879e234aaf29cd382bdee75bd0dcdc55661b
Timur Bakibayev <timurbakibayev@gmail.com>
2018-08-27 16:38:33 +0600

    Front end minimal working OK

--- front/Screens/MapScreen.js
+++ front/Screens/MapScreen.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {
     StyleSheet,
     FlatList,
+    Modal,
     View,
     Text,
     TextInput,
@@ -34,6 +35,12 @@ export default class MapScreen extends Component {
                 "longitudeDelta": 0.1098632758740905,
             },
             markers: [],
+            modalVisible: false,
+            input: {
+                name: "",
+                lat: 0,
+                lon: 0,
+            },
         };
     }
 
@@ -42,7 +49,7 @@ export default class MapScreen extends Component {
     };
 
     componentDidMount() {
-        setInterval( async ()=> {
+        setInterval(async () => {
             if (!updating) {
                 updating = true;
                 console.log("updating");
@@ -62,19 +69,20 @@ export default class MapScreen extends Component {
                     console.log(text);
                     let subjects = JSON.parse(text);
                     let newMarkers = [];
-                    subjects.forEach((subject)=> {
+                    subjects.forEach((subject) => {
                         newMarkers.push({
                             id: subject.id,
-                            title: subject.code,
+                            title: subject.subject_code,
                             description: subject.name,
                             coordinate: {latitude: subject.lat, longitude: subject.lon},
+                            ...subject
                         })
                     });
 
                     let oldMarkers = this.state.markers;
                     let changes = false;
                     newMarkers.forEach((marker) => {
-                        let correspondingOldMarker = oldMarkers.filter((mk) => mk.id===marker.id);
+                        let correspondingOldMarker = oldMarkers.filter((mk) => mk.id === marker.id);
                         if (correspondingOldMarker.length === 0) {
                             changes = true
                         } else {
@@ -88,6 +96,8 @@ export default class MapScreen extends Component {
                     if (changes) {
                         this.setState({markers: newMarkers});
                         console.log("New markers: ", newMarkers);
+                    } else {
+                        console.log("No changes");
                     }
                 }
                 updating = false;
@@ -100,20 +110,88 @@ export default class MapScreen extends Component {
         this.setState({region});
     }
 
+    moveMarker(e, marker) {
+        console.log("Moving a marker", marker);
+        console.log("Moving a marker", e.nativeEvent.coordinate);
+        let lat = e.nativeEvent.coordinate.latitude;
+        let lon = e.nativeEvent.coordinate.longitude;
+        let url = `http://navi.papers.kz/update/${marker.subject_code}/?lat=${lat}&lon=${lon}`;
+        console.log("fetching url", url);
+        fetch(
+            url,
+            {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                }
+            }
+        );
+        marker.coordinate = e.nativeEvent.coordinate;
+        this.setState({markers: [...this.state.markers.filter((mar) => mar.id !== marker.id), marker]});
+    }
+
     render() {
+        if (!this.state.modalVisible)
+            return (
+                <MapView style={{flex: 1, width: "100%", height: "100%"}}
+                         region={this.state.region}
+                         onRegionChange={this.onRegionChange.bind(this)}
+                         onLongPress={(e) => {
+                             console.log("Long press", e.nativeEvent);
+                             this.setState({
+                                 modalVisible: true, input: {
+                                     name: "name",
+                                     subject_code:  Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
+                                     lat: e.nativeEvent.coordinate.latitude,
+                                     lon: e.nativeEvent.coordinate.longitude,
+                                 }
+                             });
+                         }}
+                >
+                    {this.state.markers.map(marker => (
+                        <Marker key={marker.id}
+                                draggable
+                                coordinate={marker.coordinate}
+                                title={marker.title}
+                                description={marker.description}
+                                onDragEnd={(e) => {
+                                    this.moveMarker(e, marker)
+                                }}
+                        />
+                    ))}
+                </MapView>
+            );
         return (
-            <MapView style={{flex: 1, width: "100%"}}
-                     region={this.state.region}
-                     onRegionChange={this.onRegionChange.bind(this)}
-            >
-                {this.state.markers.map(marker => (
-                    <Marker key={marker.id}
-                        coordinate={marker.coordinate}
-                        title={marker.title}
-                        description={marker.description}
+            <View style={{marginTop: 22}}>
+                <View>
+                    <Text style={{fontSize: 24}}>Новый объект:</Text>
+                    <TextInput style={{fontSize: 24}} value={this.state.input.name}
+                               onChangeText={(text) => {
+                                   this.setState({input: {...this.state.input, name: text}});
+                                   console.log(text)
+                               }}
                     />
-                ))}
-            </MapView>
+                    <TouchableHighlight
+                        onPress={() => {
+                            let url = `http://navi.papers.kz/update/${this.state.input.subject_code}/?lat=${this.state.input.lat}&lon=${this.state.input.lon}&name=${this.state.input.name}`;
+                            console.log("fetching url", url);
+                            fetch(
+                                url,
+                                {
+                                    method: 'GET',
+                                    headers: {
+                                        'Accept': 'application/json',
+                                        'Content-Type': 'application/json',
+                                    }
+                                }
+                            );
+                            this.setState({modalVisible: false});
+                        }}>
+                        <Text style={{fontSize: 24, padding: 10, marginBottom: 100}}>Создать</Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
         );
     }
 }
